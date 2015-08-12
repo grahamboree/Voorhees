c04bc06e958754611f692cee632f187ff08a76bf
Graham Pentheny <graham.pentheny@gmail.com>
2015-08-12 01:43:51 -0400

    read float, int and string.

--- Assets/Editor/JsonReaderTest.cs
+++ Assets/Editor/JsonReaderTest.cs
@@ -4,7 +4,23 @@
 public class JsonReaderTest {
 	[Test]
 	public void SimpleRead() {
-		JsonValue one = JsonReader.Read("1");
-		Assert.That(one.Type, Is.EqualTo(JsonType.Int));
+		// int
+		JsonValue test = JsonReader.Read("1");
+		Assert.That(test.Type, Is.EqualTo(JsonType.Int));
+		Assert.That((int)test, Is.EqualTo(1));
+
+		// float
+		test = JsonReader.Read("1.5");
+		Assert.That(test.Type, Is.EqualTo(JsonType.Float));
+		Assert.That((float)test, Is.EqualTo(1.5f));
+
+		// string
+		test = JsonReader.Read("\"test\"");
+		Assert.That(test.Type, Is.EqualTo(JsonType.String));
+		Assert.That((string)test, Is.EqualTo("test"));
+
+		test = JsonReader.Read("\"\\\\\"");
+		Assert.That(test.Type, Is.EqualTo(JsonType.String));
+		Assert.That((string)test, Is.EqualTo("\\\\"));
 	}
 }
--- Assets/src/JsonReader.cs
+++ Assets/src/JsonReader.cs
@@ -1,8 +1,45 @@
 ﻿
+using System.Collections.Generic;
+
 public class JsonReader {
 	public static JsonValue Read(string json) {
-		JsonValue value = new JsonValue();
-		value = int.Parse(json);
-		return value;
+		JsonValue result = new JsonValue();
+		//Stack<JsonValue> valueStack = new Stack<JsonValue>();
+
+		for (int i = 0; i < json.Length; ++i) {
+			if (json[i] == '"') {
+				i++;
+				int startIndex = i;
+				while (!(json[i] == '"' && (json[i - 1] != '\\' || i >= 2 || json[i - 2] == '\\'))) {
+					i++;
+				}
+				return json.Substring(startIndex, i - startIndex);
+			} else if (json[i] == '-' || (json[i] <= '9' && json[i] >= '0')) {
+				int startIndex = i;
+				i++;
+				while (i < json.Length && 
+				       ((json[i] <= '9' && json[i] >= '0') ||
+						 json[i] == '.' ||
+						 json[i] == 'e' ||
+						 json[i] == 'E' ||
+						 json[i] == '-' ||
+						 json[i] == '+'))
+				{
+					i++;
+				}
+				// number
+				string numberString = json.Substring(startIndex, i - startIndex);
+				int intVal;
+				float floatVal;
+				if (int.TryParse(numberString, out intVal)) {
+					return intVal;
+				} else if (float.TryParse(numberString, out floatVal)) {
+					return floatVal;
+				} else {
+					throw new System.Exception("numer is not a number");
+				}
+			}
+		}
+		return result;
 	}
 }
