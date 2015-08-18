bb24d632261a7e7ee1fc0c3c53a3bd16684dd908
Graham Pentheny <graham.pentheny@gmail.com>
2015-08-18 00:48:08 -0400

    Read arrays
    * Restructured unit tests to be more unit-like and not have a ton of
    separate tests all grouped together in them.

--- Assets/Editor/JsonReaderTest.cs
+++ Assets/Editor/JsonReaderTest.cs
@@ -3,54 +3,196 @@
 [TestFixture]
 public class JsonReaderTest {
 	[Test]
-	public void SimpleRead() {
+	public void Int() {
 		JsonValue test;
-
 		test = JsonReader.Read("1");
 		Assert.That(test.Type, Is.EqualTo(JsonType.Int));
 		Assert.That((int)test, Is.EqualTo(1));
-		
+	}
+
+	[Test]
+	public void NegativeInt() {
+		JsonValue test;
 		test = JsonReader.Read("-1");
 		Assert.That(test.Type, Is.EqualTo(JsonType.Int));
 		Assert.That((int)test, Is.EqualTo(-1));
-		
+	}
+
+	[Test]
+	public void Float() {
+		JsonValue test;
 		test = JsonReader.Read("1.5");
 		Assert.That(test.Type, Is.EqualTo(JsonType.Float));
 		Assert.That((float)test, Is.EqualTo(1.5f));
+	}
 
+	[Test]
+	public void NegativeFloat() {
+		JsonValue test;
+		test = JsonReader.Read("-1.5");
+		Assert.That(test.Type, Is.EqualTo(JsonType.Float));
+		Assert.That((float)test, Is.EqualTo(-1.5f));
+	}
+
+	[Test]
+	public void FloatPositiveE() {
+		JsonValue test;
 		test = JsonReader.Read("1.5e+1");
 		Assert.That(test.Type, Is.EqualTo(JsonType.Float));
 		Assert.That((float)test, Is.EqualTo(15f));
+	}
 
+	[Test]
+	public void FloatE() {
+		JsonValue test;
 		test = JsonReader.Read("1.5e2");
 		Assert.That(test.Type, Is.EqualTo(JsonType.Float));
 		Assert.That((float)test, Is.EqualTo(1.5e2f));
+	}
 
+	[Test]
+	public void FloatNegativeE() {
+		JsonValue test;
 		test = JsonReader.Read("1.5e-2");
 		Assert.That(test.Type, Is.EqualTo(JsonType.Float));
 		Assert.That((float)test, Is.EqualTo(1.5e-2f));
+	}
 
-		// string
+	[Test]
+	public void NegativeFloatPositiveE() {
+		JsonValue test;
+		test = JsonReader.Read("-1.5e+1");
+		Assert.That(test.Type, Is.EqualTo(JsonType.Float));
+		Assert.That((float)test, Is.EqualTo(-15f));
+	}
+
+	[Test]
+	public void NegativeFloatE() {
+		JsonValue test;
+		test = JsonReader.Read("-1.5e2");
+		Assert.That(test.Type, Is.EqualTo(JsonType.Float));
+		Assert.That((float)test, Is.EqualTo(-1.5e2f));
+	}
+
+	[Test]
+	public void NegativeFloatNegativeE() {
+		JsonValue test;
+		test = JsonReader.Read("-1.5e-2");
+		Assert.That(test.Type, Is.EqualTo(JsonType.Float));
+		Assert.That((float)test, Is.EqualTo(-1.5e-2f));
+	}
+
+	[Test]
+	public void String() {
+		JsonValue test;
 		test = JsonReader.Read("\"test\"");
 		Assert.That(test.Type, Is.EqualTo(JsonType.String));
 		Assert.That((string)test, Is.EqualTo("test"));
+	}
 
+	[Test]
+	public void StringWithEscapedQuotes() {
+		JsonValue test;
 		test = JsonReader.Read("\"\\\\\"");
 		Assert.That(test.Type, Is.EqualTo(JsonType.String));
 		Assert.That((string)test, Is.EqualTo("\\\\"));
-
-		// Boolean
+	}
+	
+	[Test]
+	public void True() {
+		JsonValue test;
 		test = JsonReader.Read("true");
 		Assert.That(test.Type, Is.EqualTo(JsonType.Boolean));
 		Assert.That((bool)test, Is.True);
+	}
 
+	[Test]
+	public void False() {
+		JsonValue test;
 		test = JsonReader.Read("false");
 		Assert.That(test.Type, Is.EqualTo(JsonType.Boolean));
 		Assert.That((bool)test, Is.False);
+	}
 
-		// Null
+	[Test]
+	public void Null() {
+		JsonValue test;
 		test = JsonReader.Read("null");
 		Assert.That(test.Type, Is.EqualTo(JsonType.Null));
 		Assert.That(test.IsNull);
+	}
+
+	[Test]
+	public void EmptyArray() {
+		JsonValue test;
+		test = JsonReader.Read("[]");
+		Assert.That(test.Type, Is.EqualTo(JsonType.Array));
+		Assert.That(test.Count, Is.EqualTo(0));
+	}
+
+	[Test]
+	public void SimpleArray() {
+		JsonValue test;
+		test = JsonReader.Read("[1]");
+		Assert.That(test.Type, Is.EqualTo(JsonType.Array));
+		Assert.That(test.Count, Is.EqualTo(1));
+		Assert.That(test[0].Type, Is.EqualTo(JsonType.Int));
+		Assert.That((int)test[0], Is.EqualTo(1));
+	}
+
+	[Test]
+	public void MultivalueArray() {
+		JsonValue test;
+		test = JsonReader.Read("[1, 2, 3]");
+		Assert.That(test.Type, Is.EqualTo(JsonType.Array));
+		Assert.That(test.Count, Is.EqualTo(3));
+		for (int i = 0; i < 3; ++i) {
+			Assert.That(test[i].Type, Is.EqualTo(JsonType.Int));
+			Assert.That((int)test[i], Is.EqualTo(i + 1));
+		}
+	}
+
+	[Test]
+	public void NestedArray() {
+		JsonValue test;
+		test = JsonReader.Read("[[1, 2, 3]]");
+		Assert.That(test.Type, Is.EqualTo(JsonType.Array));
+		Assert.That(test.Count, Is.EqualTo(1));
+		test = test[0];
+
+		Assert.That(test.Type, Is.EqualTo(JsonType.Array));
+		Assert.That(test.Count, Is.EqualTo(3));
+		for (int i = 0; i < 3; ++i) {
+			Assert.That(test[i].Type, Is.EqualTo(JsonType.Int));
+			Assert.That((int)test[i], Is.EqualTo(i + 1));
+		}
+	}
+
+	[Test]
+	public void MultipleNestedArrays() {
+		JsonValue outer;
+		outer = JsonReader.Read("[[1, 2, 3], [4, [5]]]");
+		Assert.That(outer.Type, Is.EqualTo(JsonType.Array));
+		Assert.That(outer.Count, Is.EqualTo(2));
+		JsonValue test = outer[0];
+		
+		Assert.That(test.Type, Is.EqualTo(JsonType.Array));
+		Assert.That(test.Count, Is.EqualTo(3));
+		for (int i = 0; i < 3; ++i) {
+			Assert.That(test[i].Type, Is.EqualTo(JsonType.Int));
+			Assert.That((int)test[i], Is.EqualTo(i + 1));
+		}
+
+		test = outer[1];
+		Assert.That(test.Type, Is.EqualTo(JsonType.Array));
+		Assert.That(test.Count, Is.EqualTo(2));
+		Assert.That(test[0].Type, Is.EqualTo(JsonType.Int));
+		Assert.That((int)test[0], Is.EqualTo(4));
+
+		test = test[1];
+		Assert.That(test.Type, Is.EqualTo(JsonType.Array));
+		Assert.That(test.Count, Is.EqualTo(1));
+		Assert.That(test[0].Type, Is.EqualTo(JsonType.Int));
+		Assert.That((int)test[0], Is.EqualTo(5));
 	}
 }
--- Assets/src/JsonReader.cs
+++ Assets/src/JsonReader.cs
@@ -4,17 +4,33 @@ using System.Collections.Generic;
 public class JsonReader {
 	public static JsonValue Read(string json) {
 		JsonValue result = new JsonValue();
-		//Stack<JsonValue> valueStack = new Stack<JsonValue>();
+		Stack<JsonValue> valueStack = new Stack<JsonValue>();
 
 		for (int i = 0; i < json.Length; ++i) {
-			if (json[i] == '"') {
+			if (json[i] == '[') {
+				var array = new JsonValue();
+				array.Type = JsonType.Array;
+				valueStack.Push(array);
+			} else if (json[i] == ']') {
+				var array = valueStack.Pop();
+				if (valueStack.Count > 0) {
+					valueStack.Peek().Add(array);
+				} else {
+					result = array;
+				}
+			} else if (json[i] == '"') {
 				// string
 				i++;
 				int startIndex = i;
 				while (!(json[i] == '"' && (json[i - 1] != '\\' || i >= 2 || json[i - 2] == '\\'))) {
 					i++;
 				}
-				return json.Substring(startIndex, i - startIndex);
+				var value = new JsonValue(json.Substring(startIndex, i - startIndex));
+				if (valueStack.Count > 0) {
+					valueStack.Peek().Add(value);
+				} else {
+					result = value;
+				}
 			} else if (json[i] == '-' || (json[i] <= '9' && json[i] >= '0')) {
 				// number
 				int startIndex = i;
@@ -30,19 +46,42 @@ public class JsonReader {
 				string numberString = json.Substring(startIndex, i - startIndex);
 				int intVal;
 				float floatVal;
+				JsonValue value;
 				if (int.TryParse(numberString, out intVal)) {
-					return intVal;
+					value = intVal;
 				} else if (float.TryParse(numberString, out floatVal)) {
-					return floatVal;
+					value = floatVal;
 				} else {
 					throw new System.Exception("string '" + numberString + "' is not a number");
 				}
+				--i;
+
+				if (valueStack.Count > 0) {
+					valueStack.Peek().Add(value);
+				} else {
+					result = value;
+				}
 			} else if (json.Length - i >= 4 && json.Substring(i, 4) == "true") {
-				return new JsonValue(true);
+				var value = new JsonValue(true);
+				if (valueStack.Count > 0) {
+					valueStack.Peek().Add(value);
+				} else {
+					result = value;
+				}
 			} else if (json.Length - i >= 5 && json.Substring(i, 5) == "false") {
-				return new JsonValue(false);
+				var value = new JsonValue(false);
+				if (valueStack.Count > 0) {
+					valueStack.Peek().Add(value);
+				} else {
+					result = value;
+				}
 			} else if (json.Length - i >= 4 && json.Substring(i, 4) == "null") {
-				return new JsonValue();
+				var value = new JsonValue();
+				if (valueStack.Count > 0) {
+					valueStack.Peek().Add(value);
+				} else {
+					result = value;
+				}
 			}
 		}
 		return result;
