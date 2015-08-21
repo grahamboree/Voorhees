0c314eb152d5f05d0abdff798594b35c4fffc9da
grahamboree <graham.pentheny@gmail.com>
2015-08-21 19:02:15 -0400

    Added JsonWriter

---
+++ Assets/Editor/JsonWriterTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+[TestFixture]
+public class JsonWriterTest {
+	[Test]
+	public void WriteNull() {
+		JsonValue test = new JsonValue();
+		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("null"));
+	}
+
+	[Test]
+	public void WriteTrue() {
+		JsonValue test = true;
+		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("true"));
+	}
+
+	[Test]
+	public void WriteFalse() {
+		JsonValue test = false;
+		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("false"));
+	}
+
+	[Test]
+	public void WriteFloat() {
+		JsonValue test = 1.5f;
+		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("1.5"));
+
+		test = 1f;
+		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("1"));
+	}
+
+	[Test]
+	public void WriteInt() {
+		JsonValue test = 1;
+		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("1"));
+	}
+
+	[Test]
+	public void WriteString() {
+		JsonValue test = "test";
+		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("\"test\""));
+	}
+
+	[Test]
+	public void WriteEmptyArray() {
+		JsonValue test = new JsonValue();
+		test.Type = JsonType.Array;
+		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("[]"));
+	}
+
+	[Test]
+	public void WriteSimpleArray() {
+		JsonValue test = new JsonValue { 1, 2, 3, 4 };
+		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("[1, 2, 3, 4]"));
+	}
+
+	[Test]
+	public void WriteNestedArray() {
+		JsonValue test = new JsonValue { 1, new JsonValue { 2, 3 }, 4, 5 };
+		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("[1, [2, 3], 4, 5]"));
+	}
+
+	[Test]
+	public void WriteEmptyObject() {
+		JsonValue test = new JsonValue();
+		test.Type = JsonType.Object;
+		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("{}"));
+	}
+
+	[Test]
+	public void WriteSimpleObject() {
+		JsonValue test = new JsonValue {
+			{ "test", 1 }
+		};
+		test.Type = JsonType.Object;
+		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("{\"test\": 1}"));
+	}
+}
--- Assets/src/JsonWriter.cs
+++ Assets/src/JsonWriter.cs
@@ -1,3 +1,51 @@
-﻿
+﻿using System.Linq;
+using System.Text;
+
 public class JsonWriter {
+	public static string ToJson(JsonValue json) {
+		StringBuilder result = new StringBuilder();
+		WriteValue(result, json);
+		return result.ToString();
+	}
+
+	static void WriteValue(StringBuilder result, JsonValue value) {
+		switch (value.Type) {
+			case JsonType.Array:
+				result.Append('[');
+				for (int i = 0; i < value.Count; ++i) {
+					if (i != 0) {
+						result.Append(", ");
+					}
+					WriteValue(result, value[i]);
+				}
+				result.Append(']');
+				break;
+			case JsonType.Object:
+				result.Append('{');
+
+				// TODO remove this copy for performance.
+				var keys = value.Keys.ToList();
+
+				for (int i = 0; i < keys.Count; ++i) {
+					if (i != 0) {
+						result.Append(", ");
+					}
+					result.Append('\"');
+					result.Append(keys[i]);
+					result.Append('\"');
+					result.Append(": ");
+					WriteValue(result, value[keys[i]]);
+				}
+				result.Append('}');
+				break;
+			case JsonType.Float: result.Append((float)value); break;
+			case JsonType.Int: result.Append((int)value); break;
+			case JsonType.Boolean: result.Append((bool)value ? "true" : "false"); break;
+			case JsonType.Null: result.Append("null"); break;
+			case JsonType.String:
+				result.Append("\"");
+				result.Append((string)value);
+				result.Append("\""); break;
+		}
+	}
 }
