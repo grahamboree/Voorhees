b3ce321c0900fddc162e50f521c2820da5aa99ff
Graham Pentheny <graham.pentheny@gmail.com>
2020-12-24 15:13:58 -0500

    Clean up JsonWriter Tests

--- voorhees_tests/JsonWriterTest.cs
+++ voorhees_tests/JsonWriterTest.cs
@@ -11,66 +11,57 @@ public class JsonWriterTest {
 
 	[Test]
 	public void WriteTrue() {
-		JsonValue test = true;
-		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("true"));
+		Assert.That(JsonWriter.ToJson(true), Is.EqualTo("true"));
 	}
 
 	[Test]
 	public void WriteFalse() {
-		JsonValue test = false;
-		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("false"));
+		Assert.That(JsonWriter.ToJson(false), Is.EqualTo("false"));
 	}
 
 	[Test]
 	public void WriteFloat() {
-		JsonValue test = 1.5f;
-		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("1.5"));
-
-		test = 1f;
-		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("1"));
+		Assert.That(JsonWriter.ToJson(1.5f), Is.EqualTo("1.5"));
+		Assert.That(JsonWriter.ToJson(1f), Is.EqualTo("1"));
 	}
 
 	[Test]
 	public void WriteInt() {
-		JsonValue test = 1;
-		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("1"));
+		Assert.That(JsonWriter.ToJson(1), Is.EqualTo("1"));
 	}
 
 	[Test]
 	public void WriteString() {
-		JsonValue test = "test";
-		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("\"test\""));
+		Assert.That(JsonWriter.ToJson("test"), Is.EqualTo("\"test\""));
 	}
 
 	[Test]
 	public void WriteEmptyArray() {
-		JsonValue test = new JsonValue();
-		test.Type = JsonType.Array;
+		var test = new JsonValue {Type = JsonType.Array};
 		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("[]"));
 	}
 
 	[Test]
 	public void WriteSimpleArray() {
-		JsonValue test = new JsonValue { 1, 2, 3, 4 };
+		var test = new JsonValue { 1, 2, 3, 4 };
 		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("[1,2,3,4]"));
 	}
 
 	[Test]
 	public void WriteNestedArray() {
-		JsonValue test = new JsonValue { 1, new JsonValue { 2, 3 }, 4, 5 };
+		var test = new JsonValue { 1, new JsonValue { 2, 3 }, 4, 5 };
 		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("[1,[2,3],4,5]"));
 	}
 
 	[Test]
 	public void WriteEmptyObject() {
-		JsonValue test = new JsonValue();
-		test.Type = JsonType.Object;
+		var test = new JsonValue {Type = JsonType.Object};
 		Assert.That(JsonWriter.ToJson(test), Is.EqualTo("{}"));
 	}
 
 	[Test]
 	public void WriteSimpleObject() {
-		JsonValue test = new JsonValue {
+		var test = new JsonValue {
 			{ "test", 1 }
 		};
 		test.Type = JsonType.Object;
@@ -79,7 +70,7 @@ public class JsonWriterTest {
 
 	[Test]
 	public void WriteNestedObject() {
-		JsonValue test = new JsonValue {
+		var test = new JsonValue {
 			{
 				"test",
 				new JsonValue{
@@ -93,19 +84,19 @@ public class JsonWriterTest {
 
 	[Test]
 	public void PrettyPrintSimpleArray() {
-		JsonValue test = new JsonValue { 1, 2, 3, 4 };
+		var test = new JsonValue { 1, 2, 3, 4 };
 		Assert.That(JsonWriter.ToJson(test, true), Is.EqualTo("[\n\t1,\n\t2,\n\t3,\n\t4\n]"));
 	}
 
 	[Test]
 	public void PrettyPrintNestedArray() {
-		JsonValue test = new JsonValue { 1, new JsonValue { 2, 3 }, 4 };
+		var test = new JsonValue { 1, new JsonValue { 2, 3 }, 4 };
 		Assert.That(JsonWriter.ToJson(test, true), Is.EqualTo("[\n\t1,\n\t[\n\t\t2,\n\t\t3\n\t],\n\t4\n]"));
 	}
 
 	[Test]
 	public void PrettyPrintSimpleObject() {
-		JsonValue test = new JsonValue {
+		var test = new JsonValue {
 			{ "test", 1 },
 			{ "test2", 2 }
 		};
@@ -114,7 +105,7 @@ public class JsonWriterTest {
 
 	[Test]
 	public void PrettyPrintNestedObject() {
-		JsonValue test = new JsonValue {
+		var test = new JsonValue {
 			{ "test", 1 },
 			{ "test2", new JsonValue {
 					{ "test3", 3 },
