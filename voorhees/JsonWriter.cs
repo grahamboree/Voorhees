9f52c95fe1d69ca624bda70ffbb78f149e2805ca
Graham Pentheny <graham.pentheny@gmail.com>
2021-01-03 13:27:45 -0500

    Extract out writing methods from JsonWriter

--- voorhees/JsonWriter.cs
+++ voorhees/JsonWriter.cs
@@ -19,118 +19,117 @@ namespace Voorhees {
             }
 
             switch (value.Type) {
-                case JsonType.Array:
-                    if (JsonConfig.CurrentConfig.PrettyPrint) {
-                        result.Append("[\n");
-                        for (int i = 0; i < value.Count; ++i) {
-                            for (int j = 0; j < indentLevel + 1; ++j) {
-                                result.Append('\t');
-                            }
-
-                            WriteValue(result, value[i], indentLevel + 1);
-                            if (i < value.Count - 1) {
-                                result.Append(",\n");
-                            }
-                        }
-
-                        result.Append('\n');
-                        for (int j = 0; j < indentLevel; ++j) {
-                            result.Append('\t');
-                        }
-
-                        result.Append(']');
-                    } else {
-                        result.Append('[');
-                        for (int i = 0; i < value.Count; ++i) {
-                            if (i != 0) {
-                                result.Append(',');
-                            }
-
-                            WriteValue(result, value[i]);
-                        }
-
-                        result.Append(']');
-                    }
-
-                    break;
-                case JsonType.Object:
-                    if (JsonConfig.CurrentConfig.PrettyPrint) {
-                        result.Append("{\n");
-
-                        bool first = true;
-                        foreach (var objectPair in value as IEnumerable<KeyValuePair<string, JsonValue>>) {
-                            if (first) {
-                                first = false;
-                            } else {
-                                result.Append(",\n");
-                            }
-
-                            for (int j = 0; j < indentLevel + 1; ++j) {
-                                result.Append('\t');
-                            }
-
-                            result.Append('\"');
-                            result.Append(objectPair.Key);
-                            result.Append("\": ");
-                            WriteValue(result, objectPair.Value, indentLevel + 1);
-                        }
-
-                        result.Append("\n");
-                        for (int j = 0; j < indentLevel; ++j) {
-                            result.Append('\t');
-                        }
-
-                        result.Append('}');
-                    } else {
-                        result.Append('{');
-
-                        bool first = true;
-                        foreach (var objectPair in value as IEnumerable<KeyValuePair<string, JsonValue>>) {
-                            if (first) {
-                                first = false;
-                            } else {
-                                result.Append(",");
-                            }
-
-                            result.Append('\"');
-                            result.Append(objectPair.Key);
-                            result.Append("\":");
-                            WriteValue(result, objectPair.Value);
-                        }
-
-                        result.Append('}');
-                    }
-
-                    break;
-                case JsonType.Float:
-                    result.Append((float) value);
-                    break;
-                case JsonType.Int:
-                    result.Append((int) value);
-                    break;
-                case JsonType.Boolean:
-                    result.Append((bool) value ? "true" : "false");
-                    break;
-                case JsonType.Null:
-                    result.Append("null");
-                    break;
-                case JsonType.String: {
-                    // Replace special characters.  Do \ first because others expand into
-                    // sequences that contain \
-                    result.Append('\"');
-                    result.Append(((string) value)
-                        .Replace("\\", "\\\\")
-                        .Replace("\"", "\\\"")
-                        .Replace("/", "\\/")
-                        .Replace("\b", "\\b")
-                        .Replace("\f", "\\f")
-                        .Replace("\n", "\\n")
-                        .Replace("\r", "\\r")
-                        .Replace("\t", "\\t"));
-                    result.Append('\"');
-                }
-                    break;
+                case JsonType.Array: WriteArray(result, value, indentLevel); break;
+                case JsonType.Object: WriteObject(result, value, indentLevel); break;
+                case JsonType.Float: result.Append((float) value); break;
+                case JsonType.Int: result.Append((int) value); break;
+                case JsonType.Boolean: result.Append((bool) value ? "true" : "false"); break;
+                case JsonType.Null: result.Append("null"); break;
+                case JsonType.String: WriteString(result, value); break;
                 default: throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        /// <summary>
+        /// Replace special characters and wraps it in double quotes.
+        /// </summary>
+        /// <param name="result">Where to append the result.</param>
+        /// <param name="value">The JSON string value.</param>
+        static void WriteString(StringBuilder result, JsonValue value) {
+            // Replace \ first because other characters expand into sequences that contain \
+            result.Append('\"');
+            result.Append(((string) value)
+                .Replace("\\", "\\\\")
+                .Replace("\"", "\\\"")
+                .Replace("/", "\\/")
+                .Replace("\b", "\\b")
+                .Replace("\f", "\\f")
+                .Replace("\n", "\\n")
+                .Replace("\r", "\\r")
+                .Replace("\t", "\\t"));
+            result.Append('\"');
+        }
+
+        static void WriteObject(StringBuilder result, JsonValue value, int indentLevel) {
+            if (JsonConfig.CurrentConfig.PrettyPrint) {
+                result.Append("{\n");
+
+                bool first = true;
+                foreach (var objectPair in value as IEnumerable<KeyValuePair<string, JsonValue>>) {
+                    if (first) {
+                        first = false;
+                    } else {
+                        result.Append(",\n");
+                    }
+
+                    for (int j = 0; j < indentLevel + 1; ++j) {
+                        result.Append('\t');
+                    }
+
+                    result.Append('\"');
+                    result.Append(objectPair.Key);
+                    result.Append("\": ");
+                    WriteValue(result, objectPair.Value, indentLevel + 1);
+                }
+
+                result.Append("\n");
+                for (int j = 0; j < indentLevel; ++j) {
+                    result.Append('\t');
+                }
+
+                result.Append('}');
+            } else {
+                result.Append('{');
+
+                bool first = true;
+                foreach (var objectPair in value as IEnumerable<KeyValuePair<string, JsonValue>>) {
+                    if (first) {
+                        first = false;
+                    } else {
+                        result.Append(",");
+                    }
+
+                    result.Append('\"');
+                    result.Append(objectPair.Key);
+                    result.Append("\":");
+                    WriteValue(result, objectPair.Value);
+                }
+
+                result.Append('}');
+            }
+        }
+
+        static void WriteArray(StringBuilder result, JsonValue value, int indentLevel) {
+            if (JsonConfig.CurrentConfig.PrettyPrint) {
+                result.Append("[\n");
+                for (int i = 0; i < value.Count; ++i) {
+                    for (int j = 0; j < indentLevel + 1; ++j) {
+                        result.Append('\t');
+                    }
+
+                    WriteValue(result, value[i], indentLevel + 1);
+                    if (i < value.Count - 1) {
+                        result.Append(",\n");
+                    }
+                }
+
+                result.Append('\n');
+                for (int j = 0; j < indentLevel; ++j) {
+                    result.Append('\t');
+                }
+
+                result.Append(']');
+            } else {
+                result.Append('[');
+                for (int i = 0; i < value.Count; ++i) {
+                    if (i != 0) {
+                        result.Append(',');
+                    }
+
+                    WriteValue(result, value[i]);
+                }
+
+                result.Append(']');
             }
         }
     }
