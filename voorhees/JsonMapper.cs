949fbe4c168aecb940de2779f6a0a817327d4790
Graham Pentheny <graham.pentheny@gmail.com>
2021-01-03 19:57:27 -0500

    Change ToJson to a nested function call

--- voorhees/JsonMapper.cs
+++ voorhees/JsonMapper.cs
@@ -8,6 +8,130 @@ using System.Text;
 namespace Voorhees {
     public static class JsonMapper {
         public static string ToJson(object obj) {
+            return ToJsonImpl(obj);
+        }
+
+        public static T FromJson<T>(string jsonString) {
+            return (T) FromJson(JsonReader.Read(jsonString), typeof(T));
+        }
+
+        /////////////////////////////////////////////////
+
+        struct ObjectMetadata {
+            public bool IsDictionary;
+            public Dictionary<string, PropertyMetadata> Properties;
+
+            public Type ElementType {
+                get => element_type ?? typeof(JsonValue);
+                set => element_type = value;
+            }
+            Type element_type;
+        }
+        static readonly Dictionary<Type, ObjectMetadata> cachedObjectMetadata = new Dictionary<Type, ObjectMetadata>();
+        
+        struct PropertyMetadata {
+            public MemberInfo Info;
+            public bool IsField;
+            public Type Type;
+            public bool Ignored;
+        }
+        static readonly Dictionary<Type, List<PropertyMetadata>> typeProperties = new Dictionary<Type, List<PropertyMetadata>>();
+
+        struct ArrayMetadata {
+            public bool IsArray;
+            public int ArrayRank;
+            public bool IsList;
+
+            public Type ElementType {
+                get => element_type ?? typeof(JsonValue);
+                set => element_type = value;
+            }
+            Type element_type;
+        }
+        static readonly Dictionary<Type, ArrayMetadata> cachedArrayMetadata = new Dictionary<Type, ArrayMetadata>();
+
+        static readonly Dictionary<Type, Dictionary<Type, MethodInfo>> implicitConversionOperatorCache = new Dictionary<Type, Dictionary<Type, MethodInfo>>();
+
+        /////////////////////////////////////////////////
+
+        /// Gather property and field info about the type
+        /// Cache it so we don't have to get this info every
+        /// time we come across an instance of this type
+        static List<PropertyMetadata> GetTypePropertyMetadata(Type type) {
+            if (!typeProperties.ContainsKey(type)) {
+                var props = new List<PropertyMetadata>();
+
+                foreach (var propertyInfo in type.GetProperties()) {
+                    if (propertyInfo.Name != "Item" && !Attribute.IsDefined(propertyInfo, typeof(JsonIgnoreAttribute))) {
+                        props.Add(new PropertyMetadata {
+                            Info = propertyInfo,
+                            IsField = false
+                        });
+                    }
+                }
+
+                foreach (var fieldInfo in type.GetFields()) {
+                    if (!Attribute.IsDefined(fieldInfo, typeof(JsonIgnoreAttribute))) {
+                        props.Add(new PropertyMetadata {
+                            Info = fieldInfo,
+                            IsField = true
+                        });
+                    }
+                }
+
+                typeProperties.Add(type, props);
+            }
+
+            return typeProperties[type];
+        }
+
+        static ArrayMetadata GetCachedArrayMetadata(Type type) {
+            if (cachedArrayMetadata.ContainsKey(type)) {
+                return cachedArrayMetadata[type];
+            }
+
+            var data = new ArrayMetadata {
+                IsArray = type.IsArray,
+                ArrayRank = type.IsArray ? type.GetArrayRank() : 1,
+                IsList = type.GetInterface ("System.Collections.IList") != null
+            };
+
+            foreach (var propertyInfo in type.GetProperties()) {
+                if (propertyInfo.Name == "Item") {
+                    var parameters = propertyInfo.GetIndexParameters();
+
+                    if (parameters.Length == 1 && parameters[0].ParameterType == typeof(int)) {
+                        data.ElementType = propertyInfo.PropertyType;
+                    }
+                }
+            }
+            
+            cachedArrayMetadata.Add(type, data);
+            
+            return cachedArrayMetadata[type];
+        }
+
+        static MethodInfo GetImplicitConversionOperator(Type t1, Type t2) {
+            if (!implicitConversionOperatorCache.ContainsKey(t1)) {
+                implicitConversionOperatorCache.Add(t1, new Dictionary<Type, MethodInfo>());
+            }
+
+            if (implicitConversionOperatorCache[t1].ContainsKey(t2)) {
+                return implicitConversionOperatorCache[t1][t2];
+            }
+
+            var op = t1.GetMethod("op_Implicit", new[] {t2});
+
+            try {
+                implicitConversionOperatorCache[t1].Add(t2, op);
+            } catch (ArgumentException) {
+                return implicitConversionOperatorCache[t1][t2];
+            }
+
+            return op;
+        }
+
+        static string ToJsonImpl(object obj) {
             switch (obj) {
                 case null: return "null";
                 case JsonValue jsonValue: return JsonWriter.ToJson(jsonValue);
@@ -157,127 +281,6 @@ namespace Voorhees {
             }
             objectBuilder.Append("}");
             return objectBuilder.ToString();
-        }
-
-        public static T FromJson<T>(string jsonString) {
-            return (T) FromJson(JsonReader.Read(jsonString), typeof(T));
-        }
-
-        /////////////////////////////////////////////////
-
-        struct ObjectMetadata {
-            public bool IsDictionary;
-            public Dictionary<string, PropertyMetadata> Properties;
-
-            public Type ElementType {
-                get => element_type ?? typeof(JsonValue);
-                set => element_type = value;
-            }
-            Type element_type;
-        }
-        static readonly Dictionary<Type, ObjectMetadata> cachedObjectMetadata = new Dictionary<Type, ObjectMetadata>();
-        
-        struct PropertyMetadata {
-            public MemberInfo Info;
-            public bool IsField;
-            public Type Type;
-            public bool Ignored;
-        }
-        static readonly Dictionary<Type, List<PropertyMetadata>> typeProperties = new Dictionary<Type, List<PropertyMetadata>>();
-
-        struct ArrayMetadata {
-            public bool IsArray;
-            public int ArrayRank;
-            public bool IsList;
-
-            public Type ElementType {
-                get => element_type ?? typeof(JsonValue);
-                set => element_type = value;
-            }
-            Type element_type;
-        }
-        static readonly Dictionary<Type, ArrayMetadata> cachedArrayMetadata = new Dictionary<Type, ArrayMetadata>();
-
-        static readonly Dictionary<Type, Dictionary<Type, MethodInfo>> implicitConversionOperatorCache = new Dictionary<Type, Dictionary<Type, MethodInfo>>();
-
-        /////////////////////////////////////////////////
-
-
-        /// Gather property and field info about the type
-        /// Cache it so we don't have to get this info every
-        /// time we come across an instance of this type
-        static List<PropertyMetadata> GetTypePropertyMetadata(Type type) {
-            if (!typeProperties.ContainsKey(type)) {
-                var props = new List<PropertyMetadata>();
-
-                foreach (var propertyInfo in type.GetProperties()) {
-                    if (propertyInfo.Name != "Item" && !Attribute.IsDefined(propertyInfo, typeof(JsonIgnoreAttribute))) {
-                        props.Add(new PropertyMetadata {
-                            Info = propertyInfo,
-                            IsField = false
-                        });
-                    }
-                }
-
-                foreach (var fieldInfo in type.GetFields()) {
-                    if (!Attribute.IsDefined(fieldInfo, typeof(JsonIgnoreAttribute))) {
-                        props.Add(new PropertyMetadata {
-                            Info = fieldInfo,
-                            IsField = true
-                        });
-                    }
-                }
-
-                typeProperties.Add(type, props);
-            }
-
-            return typeProperties[type];
-        }
-
-        static ArrayMetadata GetCachedArrayMetadata(Type type) {
-            if (cachedArrayMetadata.ContainsKey(type)) {
-                return cachedArrayMetadata[type];
-            }
-
-            var data = new ArrayMetadata {
-                IsArray = type.IsArray,
-                ArrayRank = type.IsArray ? type.GetArrayRank() : 1,
-                IsList = type.GetInterface ("System.Collections.IList") != null
-            };
-
-            foreach (var propertyInfo in type.GetProperties()) {
-                if (propertyInfo.Name == "Item") {
-                    var parameters = propertyInfo.GetIndexParameters();
-
-                    if (parameters.Length == 1 && parameters[0].ParameterType == typeof(int)) {
-                        data.ElementType = propertyInfo.PropertyType;
-                    }
-                }
-            }
-            
-            cachedArrayMetadata.Add(type, data);
-            
-            return cachedArrayMetadata[type];
-        }
-
-        static MethodInfo GetImplicitConversionOperator(Type t1, Type t2) {
-            if (!implicitConversionOperatorCache.ContainsKey(t1)) {
-                implicitConversionOperatorCache.Add(t1, new Dictionary<Type, MethodInfo>());
-            }
-
-            if (implicitConversionOperatorCache[t1].ContainsKey(t2)) {
-                return implicitConversionOperatorCache[t1][t2];
-            }
-
-            var op = t1.GetMethod("op_Implicit", new[] {t2});
-
-            try {
-                implicitConversionOperatorCache[t1].Add(t2, op);
-            } catch (ArgumentException) {
-                return implicitConversionOperatorCache[t1][t2];
-            }
-
-            return op;
         }
         
         static object FromJson(JsonValue jsonValue, Type destinationType) {
