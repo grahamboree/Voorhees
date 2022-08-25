d3a1ccc17acfea1fd03e448e766cf685c1492b0b
Graham Pentheny <graham.pentheny@gmail.com>
2022-08-24 23:46:27 -0400

    Test Array JsonValue instance enumeration
    with IEnumerable<JsonValue>.GetEnumerator

--- test/JsonValueTest.cs
+++ test/JsonValueTest.cs
@@ -352,6 +352,31 @@ namespace Voorhees.Tests {
 				Assert.That((int) test[2], Is.EqualTo(3));
 			});
 		}
+
+		[Test]
+		public void GetEnumeratorAllowsIteratingThroughTheArrray() {
+			var test = new JsonValue {1, 2, 3};
+			IEnumerator<JsonValue> iterator = test.GetEnumerator();
+			Assert.Multiple(() => {
+				Assert.That(iterator.MoveNext(), Is.True);
+				Assert.That(iterator, Is.Not.Null);
+				Assert.That(iterator.Current, Is.Not.Null);
+				Assert.That(iterator.Current.Type, Is.EqualTo(JsonType.Int));
+				Assert.That((int)iterator.Current, Is.EqualTo(1));
+
+				Assert.That(iterator.MoveNext(), Is.True);
+				Assert.That(iterator.Current, Is.Not.Null);
+				Assert.That(iterator.Current.Type, Is.EqualTo(JsonType.Int));
+				Assert.That((int)iterator.Current, Is.EqualTo(2));
+				
+				Assert.That(iterator.MoveNext(), Is.True);
+				Assert.That(iterator.Current, Is.Not.Null);
+				Assert.That(iterator.Current.Type, Is.EqualTo(JsonType.Int));
+				Assert.That((int)iterator.Current, Is.EqualTo(3));
+				
+				Assert.That(iterator.MoveNext(), Is.False);
+			});
+		}
 	}
 
 	[TestFixture]
