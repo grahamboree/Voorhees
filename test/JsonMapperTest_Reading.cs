780e5c23b6e2ba4d96a90e3cd2f33cf3f721de69
Graham Pentheny <graham.pentheny@gmail.com>
2022-08-24 20:39:33 -0400

    Tests for mapping a json array to a List<T>

--- test/JsonMapperTest_Reading.cs
+++ test/JsonMapperTest_Reading.cs
@@ -253,6 +253,23 @@ namespace Voorhees.Tests {
 
             Assert.That(JsonMapper.FromJson<IntOrString[]>("[1, 2, \"three\"]"), Is.EqualTo(expected));
         }
+
+        [Test]
+        public void ReadArrayToList() {
+            var result = JsonMapper.FromJson<List<int>>("[1, 2, 3]");
+            Assert.Multiple(() => {
+                Assert.That(result, Is.Not.Null);
+                Assert.That(result.Count, Is.EqualTo(3));
+                Assert.That(result[0], Is.EqualTo(1));
+                Assert.That(result[1], Is.EqualTo(2));
+                Assert.That(result[2], Is.EqualTo(3));
+            });
+        }
+
+        [Test]
+        public void ReadArrayToNonArrayTypeThrows() {
+            Assert.Throws<Exception>(() => JsonMapper.FromJson<string>("[1,2,3]"));
+        }
     }
 
     [TestFixture]
