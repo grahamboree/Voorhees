7aabbb4aaa18d20c04687e30909085fa1039ebcc
Graham Pentheny <graham.pentheny@gmail.com>
2023-02-17 00:20:22 -0500

    Fixed benchmarkdotnet benchmark compilation

--- bench/Benchmarks.cs
+++ bench/Benchmarks.cs
@@ -8,34 +8,42 @@ namespace bench {
         string citm;
         string canada;
 
+        StringReader citm_reader;
+        StringReader canada_reader;
+
         public JsonValueReadBench() {
             citm = File.ReadAllText("../../../../../../citm_catalog.json");
             canada = File.ReadAllText("../../../../../../canada.json");
+            
+        }
+
+        [IterationSetup]
+        public void Setup_Citm() {
+            citm_reader = new StringReader(citm);
+            canada_reader = new StringReader(canada);
         }
 
         [Benchmark]
-        public Voorhees.JsonValue Read_Citm_Voorhees() => Voorhees.JsonMapper.FromJson(citm);
+        public Voorhees.JsonValue Read_Citm_Voorhees() => Voorhees.JsonMapper.FromJson(citm_reader);
 
         [Benchmark]
-        public Voorhees.JsonValue Read_Canada_Voorhees() => Voorhees.JsonMapper.FromJson(canada);
+        public Voorhees.JsonValue Read_Canada_Voorhees() => Voorhees.JsonMapper.FromJson(canada_reader);
 
         [Benchmark]
-        public LitJson.JsonData Read_Citm_LitJson() => LitJson.JsonMapper.ToObject(citm);
+        public LitJson.JsonData Read_Citm_LitJson() => LitJson.JsonMapper.ToObject(citm_reader);
 
         [Benchmark]
-        public LitJson.JsonData Read_Canada_LitJson() => LitJson.JsonMapper.ToObject(canada);
+        public LitJson.JsonData Read_Canada_LitJson() => LitJson.JsonMapper.ToObject(canada_reader);
 
         [Benchmark]
         public Newtonsoft.Json.Linq.JObject Read_Citm_NewtonSoft() {
-            using var sr = new StringReader(citm);
-            var reader = new Newtonsoft.Json.JsonTextReader(sr);
+            var reader = new Newtonsoft.Json.JsonTextReader(citm_reader);
             return Newtonsoft.Json.Linq.JObject.Load(reader);
         }
 
         [Benchmark]
         public Newtonsoft.Json.Linq.JObject Read_Canada_NewtonSoft() {
-            using var sr = new StringReader(canada);
-            var reader = new Newtonsoft.Json.JsonTextReader(sr);
+            var reader = new Newtonsoft.Json.JsonTextReader(canada_reader);
             return Newtonsoft.Json.Linq.JObject.Load(reader);
         }
     }
@@ -43,35 +51,32 @@ namespace bench {
     [MemoryDiagnoser]
     [MarkdownExporterAttribute.GitHub]
     public class JsonValueWriteBench {
-        string citm;
-        string canada;
-        
         Voorhees.JsonValue voorhees_canada;
         Voorhees.JsonValue voorhees_citm;
 
-        public JsonValueWriteBench() {
-            citm = File.ReadAllText("../../../../../../citm_catalog.json");
-            canada = File.ReadAllText("../../../../../../canada.json");
-            
-            voorhees_citm = Voorhees.JsonMapper.FromJson(citm);
-            voorhees_canada = Voorhees.JsonMapper.FromJson(canada);
+        [IterationSetup(Target = nameof(Write_Citm_Voorhees))]
+        public void Setup_Citm_Voorhees() {
+            voorhees_citm = Voorhees.JsonMapper.FromJson(new StreamReader(File.OpenRead("../../../../../../citm_catalog.json")));
         }
 
-        #region Voorhees citm
         [Benchmark]
         public string Write_Citm_Voorhees() => Voorhees.JsonMapper.ToJson(voorhees_citm);
-        #endregion
-        
-        #region Voorhees canada
+
+        [IterationSetup(Target = nameof(Write_Canada_Voorhees))]
+        public void Setup_Canada_Voorhees() {
+            voorhees_canada = Voorhees.JsonMapper.FromJson(new StreamReader(File.OpenRead("../../../../../../canada.json")));
+        }
+
         [Benchmark]
         public string Write_Canada_Voorhees() => Voorhees.JsonMapper.ToJson(voorhees_canada);
-        #endregion
         
-        #region LitJson citm
+        #region LitJson
         LitJson.JsonData litjson_citm;
-        
+        LitJson.JsonData litjson_canada;
+
         [IterationSetup(Target = nameof(Write_Citm_LitJson))]
         public void Setup_Citm_LitJson() {
+            var citm = File.ReadAllText("../../../../../../citm_catalog.json");
             litjson_citm = LitJson.JsonMapper.ToObject(citm);
         }
         
@@ -82,13 +87,10 @@ namespace bench {
             litjson_citm.ToJson(writer);
             return textWriter.ToString();
         }
-        #endregion
-
-        #region LitJson canada
-        LitJson.JsonData litjson_canada;
         
         [IterationSetup(Target = nameof(Write_Canada_LitJson))]
         public void Setup_Canada_LitJson() {
+            var canada = File.ReadAllText("../../../../../../canada.json");
             litjson_canada = LitJson.JsonMapper.ToObject(canada);
         }
         
@@ -103,9 +105,11 @@ namespace bench {
         
         #region Newtonsoft citm
         Newtonsoft.Json.Linq.JObject newtonsoft_citm;
+        Newtonsoft.Json.Linq.JObject newtonsoft_canada;
         
         [IterationSetup(Target = nameof(Write_Citm_Newtonsoft))]
         public void Setup_Citm_Newtonsoft() {
+            var citm = File.ReadAllText("../../../../../../citm_catalog.json");
             newtonsoft_citm = Newtonsoft.Json.Linq.JObject.Load(new Newtonsoft.Json.JsonTextReader(new StringReader(citm)));
         }
 
@@ -116,13 +120,10 @@ namespace bench {
             newtonsoft_citm.WriteTo(writer);
             return textWriter.ToString();
         }
-        #endregion
-
-        #region Newtonsoft canada
-        Newtonsoft.Json.Linq.JObject newtonsoft_canada;
         
         [IterationSetup(Target = nameof(Write_Canada_Newtonsoft))]
         public void Setup_Canada_Newtonsoft() {
+            var canada = File.ReadAllText("../../../../../../canada.json");
             newtonsoft_canada = Newtonsoft.Json.Linq.JObject.Load(new Newtonsoft.Json.JsonTextReader(new StringReader(canada)));
         }
         
